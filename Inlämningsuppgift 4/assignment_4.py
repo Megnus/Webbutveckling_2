c9c1ae76a71c51a5aa51574e594bc3747b4d446d
megnus <megnus@hotmail.com>
2016-09-05 18:30:40 +0200

    Inlämningsuppgift 4 done

--- Inlämningsuppgift 4/assignment_4.py
+++ Inlämningsuppgift 4/assignment_4.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template("index.html")
 
 
 @app.route('/contact')
@@ -15,13 +15,18 @@ def contact():
 
 @app.route('/movies')
 def movies():
-    movies = ["Godfather", "Hudson Hawk", "Die Hard"]
-    return render_template("movies.html", movies=movies)
+    titles = ["Godfather", "Hudson Hawk", "Die Hard"]
+    return render_template("movies.html", titles=titles)
 
 
-@app.route('/hello/<username>')
-def hello(username):
-    return 'Hello there %s' % username
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template("hello.html", name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 
 if __name__ == "__main__":
