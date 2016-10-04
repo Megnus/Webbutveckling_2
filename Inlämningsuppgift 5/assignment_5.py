4853a09439604d67032674f3094a783c79573faa
megnus <megnus@hotmail.com>
2016-10-04 18:34:41 +0200

    Working on assignment 5 (1)

---
+++ Inlämningsuppgift 5/assignment_5.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, render_template, jsonify
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.route('/')
+def index():
+    print('yoyoyo')
+    return render_template("index.html")
+
+
+@app.route('/api', methods=['POST'])
+def api():
+    print('yahoo')
+    print(request.form['firstName']);
+    # return jsonify({'tasks': tasks})
+    return render_template("index.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/movies')
+def movies():
+    print('yoyoyo')
+    titles = ["Godfather", "Hudson Hawk", "Die Hard"]
+    return render_template("movies.html", titles=titles)
+
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template("hello.html", name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
