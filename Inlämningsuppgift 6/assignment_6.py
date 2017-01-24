46e48a12fb64039682c3e86879ae86b27d617d04
megnus <megnus@hotmail.com>
2017-01-24 16:20:01 +0100

    Working on assignment 6 (3)

--- Inlämningsuppgift 6/assignment_6.py
+++ Inlämningsuppgift 6/assignment_6.py
@@ -4,7 +4,6 @@
     2017-01-20
 """
 
-
 from flask import Flask, request, render_template, jsonify, json
 
 app = Flask(__name__)
@@ -14,66 +13,59 @@ class Person:
     def __init__(self, name, age=None, sex=None):
         self.name, self.age, self.sex = name, age, sex
 
+
 @app.route('/')
 def index():
     return render_template('index.html', article=None)
 
 
-@app.route('/done', methods=['POST', 'GET'])
-def done():
+@app.route('/add')
+def add():
+    return render_template('add.html', article=None)
+
+
+@app.route('/added', methods=['POST', 'GET'])
+def added():
     if request.method == 'POST':
         add_article(request.form['author'],
                     request.form['title'],
                     request.form['article'])
-        # is_edit
     return render_template('done.html')
 
 
 @app.route('/edited', methods=['POST', 'GET'])
 def edited():
     if request.method == 'POST':
-        add_article(request.form['author'],
-                    request.form['title'],
-                    request.form['article'])
-        # is_edit
+        edit_article(request.form['author'],
+                     request.form['title'],
+                     request.form['article'])
     return render_template('done.html')
-
-@app.route('/edit')
-def edit():
-    return render_template('edit.html', setter=Person('snn', 25, 6))
 
 
 @app.route('/edit/<title>')
-def edit_article(title):
-    return render_template('edit.html', article=get_articlec(title))
+def edit(title):
+    return render_template('edit.html', article=get_article(title))
 
 
 @app.route('/article/')
-@app.route('/article/<ssn>')
-def get_article(title):
-    articles = read_articles()['articles']
-    for i in range(0, len(articles)):
-        if articles[i]['title'] == title:
-            return jsonify({'title': articles[i]})
-    return render_template('page_not_found.html'), 404
-
-
-def get_articlec(title):
-    articles = read_articles()['articles']
-    for i in range(0, len(articles)):
-        if articles[i]['title'] == title:
-            return articles[i]
-    return render_template('page_not_found.html'), 404
+@app.route('/article/<title>')
+def article(title):
+    return render_template('read.html', article=get_article(title))
 
 
 @app.route('/articles')
-def get_articles():
+def articles():
     return render_template('articles.html', articles=read_articles()['articles'])
 
 
 @app.route('/api')
-def users():
+def get_api():
     return jsonify(read_articles())
+
+
+@app.route('/api/<title>')
+def get_api_title(title):
+    return jsonify(get_article(title))
 
 
 @app.errorhandler(404)
@@ -81,35 +73,60 @@ def page_not_found(error):
     return render_template('page_not_found.html'), 404
 
 
+def get_json_article(title):
+    content = read_articles()['articles']
+    for i in range(0, len(content)):
+        if content[i]['title'] == title:
+            return jsonify({'title': content[i]})
+    return render_template('page_not_found.html'), 404
+
+
+def get_article(title):
+    content = read_articles()['articles']
+    for i in range(0, len(content)):
+        if content[i]['title'] == title:
+            return content[i]
+    return render_template('page_not_found.html'), 404
+
+
 def read_articles():
-    articles = {'articles': []}
+    content = {'articles': []}
     # noinspection PyBroadException
     try:
         file = open('articles.json', 'r')
-        articles = json.loads(file.read())
+        content = json.loads(file.read())
         file.close()
     except:
         print("Could not read file or file is empty")
-    return articles
+    return content
 
 
-def write_articles(articles):
+def write_articles(content):
     # noinspection PyBroadException
     try:
         file = open('articles.json', 'w')
-        file.write(json.dumps(articles))
+        file.write(json.dumps(content))
         file.close()
     except:
         print("Could not read file")
 
 
-def add_article(author, title, article):
-    articles = read_articles()
-    articles['articles'].append({'author': author,
-                                 'title': title,
-                                 'article': article})
+def add_article(author, title, text):
+    content = read_articles()
+    content['articles'].append({'author': author,
+                                'title': title,
+                                'article': text})
     # created, edited, revision, author: firstname, last name,
-    write_articles(articles)
+    write_articles(content)
+
+
+def edit_article(author, title, text):
+    content = read_articles()
+    for i in range(0, len(content['articles'])):
+        if content['articles'][i]['title'] == title:
+            content['articles'][i]['author'] = author;
+            content['articles'][i]['article'] = text;
+    write_articles(content)
 
 
 if __name__ == "__main__":
